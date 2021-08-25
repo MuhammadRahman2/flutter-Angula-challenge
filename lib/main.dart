56c63e602ff4c7bad243fd666cc6cd5320828ef5
MuhammadRahman2 <muhammadrahmna2042@gmail.com>
2021-08-25 11:26:48 +0500

    Bussiness Card Compete

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bussness Card',
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child:Column( 
+            mainAxisAlignment:  MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('Assets/Images/mr.jpg'),
+              ),
+              Text('Muhammad Rahman',style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize:  35.0,
+                color: Colors.white
+
+              ),),
+              SizedBox(
+                width: 180.0,
+                height: 20.0,
+                child: Divider(
+                 color:  Colors.teal.shade100,
+                )
+
+              ),
+              Text("Flutter Developer",style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                color:  Colors.teal.shade100
+              ),),
+              Card(
+                margin: EdgeInsets.symmetric( vertical: 10.0, horizontal: 25.0),
+                child: ListTile (
+                   leading: Icon(Icons.phone),
+                   title: Text('+923029462604',
+                   ),
+                   ) ,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric( vertical: 10.0, horizontal: 25.0),
+                child: ListTile (
+                   leading: Icon(Icons.email),
+                   title: Text('muhammadrahman2042@gmail.com'
+                   ),
+                         
+                   ) ,
+              )
+            ],
+          ),
+          ),
+      ),
+    );
+  }
 }
- 
- class MyApp extends StatelessWidget {
-   const MyApp({ Key? key }) : super(key: key);
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       home: Scaffold(
-         backgroundColor: Colors.teal,
-         body: SafeArea(
-           child: Row(
-             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-             children: [
-               Container(
-                 color: Colors.red,
-                 width: 100.0,
-               ),
-               Column(
-                 mainAxisAlignment:  MainAxisAlignment.center,
-                 children: [              
-                   Container(
-                     width: 100.0,
-                     height: 100.0,
-                     color: Colors.yellow,
-                   ),
-                   Container(
-                     width: 100.0,
-                     height: 100.0,
-                     color: Colors.green,
-                   ),
-                 ],
-               ),
-               Container(
-                     width: 100.0,
-                     color: Colors.blue,
-                   ),
-             ],
-           ),
-         ),
-       ),
-     );
-   }
- }
